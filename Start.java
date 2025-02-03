737c23eaf603a3ada05bf2f53bdc85b068198c16
Atiqul Islam Sakib <sakibalsaud1@gmail.com>
2025-02-03 09:48:10 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import javax.swing.*;
+import Frame.*;
+import Classes.*;
+
+public class Start {
+    public static void main(String[] args) {
+        LoginFrame l1 = new LoginFrame();
+        l1.setVisible(true);
+    }
+}
